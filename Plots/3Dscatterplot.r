b2b819b0e847b473d45566ce56d1aa5e2d8f6767
Rehan Zafar Paracha <rehanzfr@gmail.com>
2019-12-27 04:14:46 +0500

    Update 3Dscatterplot.r

--- Plots/3Dscatterplot.r
+++ Plots/3Dscatterplot.r
@@ -7,7 +7,7 @@ library("scatterplot3d")
 # For Colors Visit http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
 colors <- c("chartreuse", "#E69F00", "#56B4E9")
 
-# Lets Import Data (Its just like you have your data in csv file so think like it)
+# Lets Import Data (You can import your csv after saving it from Excel)
 imported_data <- read.csv("IRIS.csv", header = TRUE)
 
 # Iris dataset
@@ -16,24 +16,24 @@ x <- imported_data$Sepal.Width
 y <- imported_data$Petal.Length
 
 # Make Sctterplot
-S3D <- scatterplot3d(x, y, z, 
-                     highlight.3d=TRUE, 
-                     col.axis="blue",
-                     col.grid="lightblue",
-                     col.lab="royalblue",
-                     cex.axis =1, 
-                     cex.lab=1,
-                     font.axis=2 , 
-                     font.lab=1,
-                     lty.axis=2, 
-                     lty.grid=2,
-                     #color=colors[grps],                          # Will be ignored if highlight.3d = TRUE
+S3D <- scatterplot3d(x, y, z,
                      main="3D Scatter Plot",
                      sub="Subtitle",
-                     pch=shapes,
                      xlab="X-axis", 
                      ylab="Y-axis", 
                      zlab="Z-axis",
+                     #highlight.3d=TRUE, 
+                     #col.axis="blue",
+                     #col.grid="lightblue",
+                     #col.lab="royalblue",
+                     #cex.axis =1, 
+                     #cex.lab=1,
+                     #font.axis=2 , 
+                     #font.lab=1,
+                     #lty.axis=2, 
+                     #lty.grid=2,
+                     #color=colors[grps],                          # Will be ignored if highlight.3d = TRUE
+                     #pch=shapes,
                      #xlim=c(-1,2), 
                      #ylim=c(-1,2),
                      #zlim=c(-9, 10),
@@ -48,17 +48,19 @@ S3D <- scatterplot3d(x, y, z,
                      #lab=c(-1,1,1),
                      #lab.z=
                      #lwd=5,
-                     mar = c(5, 5, 4, 3)
+                     #mar = c(5, 5, 4, 3)
 )
 
-S3D.coords <- S3D$xyz.convert(x, y, z)
 
-# Add labels to the points (optional)
+
+
+# Add labels to the points (OPTIONAL)
+S3D.coords <- S3D$xyz.convert(x, y, z)
 text(S3D.coords$x, S3D.coords$y,                                   # x and y coordinates
      labels=iris$Species,                                          # text on plot
      cex=.5, pos=4)                                        
 
-# add the legend
+# add the legend (OPTIONAL)
 legend("topleft",                                                  # topleft, right, bottom
        inset=.05,
        legend = levels(iris$Species),
@@ -72,7 +74,7 @@ legend("topleft",                                                  # topleft, ri
        horiz = FALSE
 )
 
-# Add regression plane (optional)
+# Add regression plane (OPTIONAL)
 lm <- lm(z ~ x + y)
 S3D$plane3d(lm, draw_polygon = TRUE, draw_lines = FALSE, 
             polygon_args = list(col = adjustcolor( "slategray1", alpha.f = 0.2)),lty= "dotted" , lwd=0.7)
